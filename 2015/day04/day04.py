eca8e191580f63137e154f162635c4663ad9e72b
Eigil Skjæveland <eigil@skjaeve.land>
2016-12-01 09:21:21 +0100

    Clean for 2016.

--- 2015/day04/day04.py
+++ 2015/day04/day04.py
@@ -2,6 +2,6 @@ from hashlib import md5
 
 for n in 5, 6:
     i = 0
-    while '0'*n != md5(b'iwrupvqb%d' % i).hexdigest()[:n]:
+    while '0'*n != md5(b'bgvyzdsv%d' % i).hexdigest()[:n]:
         i += 1
     print(i)
